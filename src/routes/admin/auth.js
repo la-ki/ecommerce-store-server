a6599790743fc03525de8a41ce88df6b197a56cc
la-ki <layarkostic94@gmail.com>
2020-10-08 18:51:32 +0200

    Added Signout controller and route

--- src/routes/admin/auth.js
+++ src/routes/admin/auth.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { signup, signin } = require('../../controller/admin/auth');
+const { signup, signin, signout } = require('../../controller/admin/auth');
 const {isRequestValidated, validateSignupRequest, validateSigninRequest} =require('../../validators/auth');
+const {requireSignin} = require('../../middlewares');
 const router = express.Router();
 
 
 router.post('/admin/signup', validateSignupRequest, isRequestValidated, signup);
 router.post('/admin/signin',validateSigninRequest, isRequestValidated, signin);
+router.post('/admin/signout', signout);
 
 
 
